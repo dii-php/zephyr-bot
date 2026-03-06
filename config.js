223d4e41636ebf8ee29dc5e5a52c1914ba07d7d3
Alediii <73929515+dii-php@users.noreply.github.com>
2026-03-07 00:22:07 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    token: process.env.TOKEN,
+    clientId: process.env.CLIENT_ID,
+    guildId: process.env.GUILD_ID,
+    mongoURI: process.env.MONGO_URI,
+    voiceChannelId: process.env.VOICE_CHANNEL_ID
+};
